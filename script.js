d18f0ecfa38df9cb6c4c75163564afe9f5f8ed8e
Anhvtq3 <83685681+Anhvtq3@users.noreply.github.com>
2021-05-07 10:50:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("RUN SCRIPT");
+
+// Đổi style của tiêu đề Our companies thành chữ in đậm và kích thước 32px
+// Gợi ý: Sử dụng .style.fontWeight và .style.fontSize
+
+// Đổi màu nền của phần footer thành màu #0984e3
+// Gợi ý: Sử dụng .style.backgroundColor
+
+// Đổi đoạn text ở footer từ "© 2020 One Mount Group, JSC." thành "© 2021 One Mount Group, JSC."
+// Gợi ý: Sử dụng .innerText
+
+// Đổi ảnh đầu tiên thành ảnh onhousing.png
+// Gợi ý: Sử dụng .src
+
+
+const textContentOutput = document.getElementById("tentapdoan");
+// textContentOutput.value = source.textContent;
+textContentOutput.innerText = "© 2021 One Mount Group, JSC.";
+
